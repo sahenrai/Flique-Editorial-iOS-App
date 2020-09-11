a1ad6ddbc41a3505e1ebb2c6f99c27977879831c
Sahen Rai <sahen.rai@gmail.com>
2020-09-10 20:26:18 -0500

    Initial Commit

---
+++ FliqueEditorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FliqueEditorial
+//
+//  Created by Sahen Rai on 9/10/20.
+//  Copyright © 2020 Sahen Rai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
